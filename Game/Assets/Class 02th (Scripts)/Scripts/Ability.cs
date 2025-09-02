82296e929f2b3bf2a988de5a916f84d2b2074314
DMiniKim <angks1212@naver.com>
2025-09-02 15:41:07 +0900

    (2025.9.2) 수출용 패키지

---
+++ Game/Assets/Class 02th (Scripts)/Scripts/Ability.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Ability: MonoBehaviour
+{
+    private int strength;
+
+    void Start()            // »ý¼ºÀÚ
+    {
+        strength = 10;    
+        Debug.Log("Strength : " +  strength);
+    }
+
+    void Update()
+    {
+        
+    }
+}
