4ff33450b60ebc9010f5fc5536371b6bf141f9be
DMiniKim <angks1212@naver.com>
2025-09-17 15:46:54 +0900

    (2025.9.17) 코루틴

--- Game/Assets/Class 09th ( Instantiate )/Scripts/CreateManager.cs
+++ Game/Assets/Class 09th ( Instantiate )/Scripts/CreateManager.cs
@@ -1,6 +1,8 @@
+using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
+using TMPro.EditorUtilities;
 using UnityEngine;
+
 
 public class CreateManager : MonoBehaviour
 {
@@ -13,16 +15,11 @@ public class CreateManager : MonoBehaviour
     private void Start()
     {
         Initialize();
+        StartCoroutine(RandomIndex());
     }
     private void Update()
     {
-        float eventTime = 5.0f;
-        time += Time.deltaTime;
-        if (time >= eventTime)
-        {
-            Appear();
-            time = 0.0f;
-        }
+       
     }
 
     void Appear()
@@ -49,6 +46,53 @@ public class CreateManager : MonoBehaviour
             unitlist.Add(clone);
         }
     }
+    IEnumerator Coroutine()  // 코루틴 함수
+    { 
+        Debug.Log("Coroutine Start");
+
+        yield return new WaitForSeconds(5.0f);
+
+        Debug.Log("Coroutine End");
+    }
+    // 이걸 쓰기 위해선 DeleyedCallManager.cs 가 필요하다.
+    // StartedCoroutine(Couroutine()); 이런 느낌.
 
 
+    IEnumerator RandomIndex()
+    {
+        float cooltime = 1.0f + (time * 0.1f);
+        
+        WaitForSeconds wait = new WaitForSeconds(5.0f* cooltime);
+        while (true)
+        {
+            yield return wait;
+
+            int index = Random.Range(0, unitlist.Count);
+            if (unitlist[index].activeSelf == true)
+            { 
+                index++;
+                if (index >= unitlist.Count) index = 0;
+                unitlist[index].SetActive(true);
+            }
+            else
+            {
+                unitlist[index].SetActive(true);
+                Debug.Log("Unit Appear");
+            }
+            int count = 0;
+            for (int i = 0; i < unitlist.Count; i++)
+            {
+                if (unitlist[i].activeSelf == true) count++;
+            }
+            if (count == unitlist.Count)
+            {
+                Debug.Log("All Unit Appear");
+                yield break;
+            }
+        }
+    }
 }
+// 5초마다 인덱스를 받음.
+// 인덱스에 접근해서 활성화 
+// 활성화 되있으면 인덱스 +1 활성화
+// 전부 활성화 되어있을 시 반복문 종료 break;
